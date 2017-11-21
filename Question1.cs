aa1a13742656811e779b514a1af78808f2405c2e
Aneres <serena0aneres@gmail.com>
2017-11-21 22:09:27 +0000

    Add files via upload

---
+++ Question1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PopAssignment
+{
+    class Question1
+    {
+        static void Main(string[] args)
+        {
+            //Declare variables
+            decimal grossPay, taxPayable, netPay;
+
+            //Request input from user
+            Console.WriteLine("Enter your Gross Pay: ");
+            grossPay = decimal.Parse(Console.ReadLine());
+
+            //Calculate Taxable Amount, Tax Payable and Net Pay
+            if (grossPay < 10000)
+            {
+                Console.WriteLine("No tax will be deducted.\n Your Net Pay is {0:0.00} euros", grossPay);
+            }
+            else if (grossPay > 10000 && grossPay < 50000)
+            {
+                taxPayable = ((grossPay - 10000) * 20 / 100);
+                netPay = grossPay - taxPayable;
+                Console.WriteLine("Tax on {0} euros is {1:0.00} euros. \n Net Pay is {2:0.00} euros", grossPay, taxPayable, netPay);
+            }
+
+            else if (grossPay > 50000)
+            {
+                taxPayable = ((grossPay - 50000) * 40 / 100) + (40000 * 20 / 100);
+                netPay = grossPay - taxPayable;
+                Console.WriteLine("Tax on {0} euros is {1:0.00} euros. \n NetPay is {2:0.00}", grossPay, taxPayable, netPay);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Question2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PopAssignment
+{
+    class Question2
+    {
+        static void Main()
+        {   //declare variables
+            double number, highestNumber = 0, lowestNumber = 100, evenCount = 0, sum = 0;
+            decimal average;
+
+            //Request numbers from user
+            do
+            {
+                Console.Write("Enter a number between 1 and 100: ");
+                number = double.Parse(Console.ReadLine());
+
+            //Check validity of numbers input
+                if (number < 1 || number > 100)
+                {
+                    break;
+                }
+
+                if (number % 2 == 0)
+                {
+                    sum = number + sum                                                                                                                  
+                    evenCount++;
+
+                    if (number > highestNumber)
+
+                        highestNumber = number;
+
+                    if (number < lowestNumber)
+
+                        lowestNumber = number;
+                }
+                 
+            } while (number >= 1 && number <= 100);
+
+            average =(decimal) sum / (decimal) evenCount;
+            Console.WriteLine("\nThe number of even numbers entered:{0}", evenCount);
+            Console.WriteLine("The sum of even numbers entered is: {0}", sum);
+            Console.WriteLine("The average of even numbers entered is: {0:00.00}",average);
+            Console.WriteLine("The highest number entered is: {0} ", highestNumber);
+            Console.WriteLine("The lowest number entered is: {0}", lowestNumber);
+
+            Console.Read();
+        }
+    }
+}
